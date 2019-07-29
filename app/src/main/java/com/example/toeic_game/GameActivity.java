da4343e496f6e784e58b7eb3377954eeac1c1dd2
EternalFlyn <EternalFlyn@gmail.com>
2019-07-29 20:36:38 +0800

    Optimize the RAM
    using animator to avoid setTextSize bug

--- app/src/main/java/com/example/toeic_game/GameActivity.java
+++ app/src/main/java/com/example/toeic_game/GameActivity.java
@@ -1,22 +1,20 @@
 package com.example.toeic_game;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 public class GameActivity extends AppCompatActivity {
 
     private int time;
-    private float textScale = 1;
     private CountDownView cdv;
     private TextView text;
     private Button b;
@@ -34,8 +32,6 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        text.setText("3");
-        text.setTextSize(72);
         cdv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,44 +46,26 @@ public class GameActivity extends AppCompatActivity {
         });
     }
 
-    private void Ready() {
-        if(clock != null) {
-            clock.cancel();
-        }
-        textScale = 1;
+    public void Ready() {
         time = 3;
-        text.setText("3");
-        clock = new CountDownTimer(3100,40) {
-
+        final AnimatorSet animation = (AnimatorSet) AnimatorInflater.loadAnimator(GameActivity.this, R.animator.text_countdown);
+        animation.setTarget(text);
+        if(clock != null)
+            clock.cancel();
+        clock = new CountDownTimer(3010,1000) {
             @Override
             public void onFinish() {
                 text.setText("Start!!");
-                text.setTextSize(36);
             }
 
             @Override
             public void onTick(long millisUntilFinished) {
-                setText();
+                text.setText(String.valueOf(time));
+                animation.start();
+                time -= 1;
             }
-
-
         };
         clock.start();
-    }
-
-    private void setText() {
-        if (textScale > 0) {
-            textScale -= 0.04;
-            System.out.println("a");
-        }
-        else {
-            textScale = 1;
-            if (time > 1) {
-                time--;
-                text.setText(String.valueOf(time));
-            }
-        }
-        text.setTextSize(textScale * 72);
     }
 
 }
